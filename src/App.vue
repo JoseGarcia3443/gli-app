354b1b9bb3bf60fca6114dba76d6c6ff749633a1
Jose Garcia <josej19g@gmail.com>
2021-10-27 20:11:24 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
-<template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+<template lang="pug">
+#app
+  router-view
 </template>
 
 <style lang="scss">
+@import "./style/simple-grid/bootstrap-grid.scss";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
